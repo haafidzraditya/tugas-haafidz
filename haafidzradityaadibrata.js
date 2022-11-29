ac471e09c0fa092c3e3cce47293d62682776993d
haafidzraditya <118706652+haafidzraditya@users.noreply.github.com>
2022-11-29 11:37:33 +0700

    Add files via upload

---
+++ haafidzradityaadibrata.js
@@ -0,0 +1,10 @@
+var p = 5
+
+for (p = 0; p < 10; p++)
+    if (p % 2 == 1)
+        console.log(p + ' adalah ganjil')
+    else {
+        p % 2 == 0
+        console.log(p + ' adalah genap')
+    }
+ 
